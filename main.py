5fffb3103796573e263d55b737c116e992f21f13
mike <mike@mike-desktop.(none)>
2012-02-01 14:05:11 -0300

    base interfaz gui maker

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#       
+#       Copyright 2011 Jonathan Ferreyra <jalejandroferreyra@gmail.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+
+from gui import GUI
+
+GUI()
+
+#TODO: no agrega qdialog al py
+#TODO: no agrega qdialog al py(2)
+#TODO: no agrega el nombre del ui al py
+#TODO: no queda bien armado el ui en qdialog
+#TODO: si hay boton guardar que te permita agregar el metodo guardar
+#TODO: crear un metodo extractor de datos
+#TODO: titulo en el dialogo
+#TODO: agregar spinbox
