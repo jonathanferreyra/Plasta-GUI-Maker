6ae77943566ef7aa95c6572327de1644a1b9a475
Jonathan Ferreyra <jalejandroferreyra@gmail.com>
2012-02-16 18:06:27 -0300

    cambios en api

--- api/gui_maker.py
+++ api/gui_maker.py
@@ -21,202 +21,16 @@
 import os.path
 import shutil
 import pathtools
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <todo>
-#~ 1- Nombre clase o tipo de Ventana [MainWindow, Dialog]
-#~ 2- Ancho y Alto
-#~ 3- Widgets
-
-todo = '''<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>###</class>
- <widget class="Q###" name="###">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>%ancho%</width>
-    <height>%alto%</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>###</string>
-  </property>
-  
-  <widget class="QWidget" name="centralwidget">
-   <layout class="QVBoxLayout" name="verticalLayout">
-   %widgets%
-   </layout>
-  </widget>
-  
- </widget>
- <resources/>
- <connections/>
-</ui>'''
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <par_label_layout>
-#~ 1- Nombre del layout
-#~ 2- Nombre del label
-#~ 3- Ancho del label
-#~ 4- Etiqueta del label
-#~ 5- Widget
-
-par_label_layout = '''<item>
-     <layout class="QHBoxLayout" name="hl###"><!-- Cambio -->
-      <item>
-       <widget class="QLabel" name="lb###"><!-- Cambio -->
-        <property name="minimumSize">
-         <size>
-          <width>%d</width><!-- aca va el nombre mas grande x 8 -->
-          <height>0</height>
-         </size>
-        </property>
-        <property name="text">
-            <string>###</string><!-- Cambio -->
-        </property>
-        <property name="alignment">
-            <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      %s
-     </layout>
-    </item>'''
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <par_botones>
-#~ 1- Nombre del boton1
-#~ 2- Nombre del boton2
-
-par_botones = '''<item>
-     <layout class="QHBoxLayout" name="horizontalLayout">
-      <item>
-       <spacer name="horizontalSpacer">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>40</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item>
-       <widget class="QPushButton" name="bt###">
-        <property name="text">
-         <string>###</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QPushButton" name="bt%%%">
-        <property name="text">
-         <string>%%%</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </item>'''
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <par_botones>
-#~ 1- Nombre del boton al que le asigna el evento
-#~ 2- Tipo de ventana
-
-evento_cerrar_ventana = '''<connections>
-  <connection>
-   <sender>bt###</sender>
-   <signal>clicked()</signal>
-   <receiver>%%%</receiver>
-   <slot>close()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>273</x>
-     <y>201</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>123</x>
-     <y>186</y>
-    </hint>
-   </hints>
-  </connection>
- </connections>'''
-     
-#~ Orden de los valores que se reemplazaran en la plantilla: <lineedit>
-#~ 1- Nombre del entry
-
-lineedit = '''<item>
-       <widget class="QLineEdit" name="e###"><!-- Cambio -->
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>'''
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <combobox>
-#~ 1- Nombre del combo
-
-combobox = '''<item>
-       <widget class="QComboBox" name="cb###"><!-- Cambio -->
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>'''
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <lineedit>
-#~ 1- Nombre del radiobutton
-#~ 2- Etiqueta del radiobutton
-
-radiobutton = '''<item>
-       <widget class="QRadioButton" name="rb###">
-        <property name="text">
-         <string>###</string>
-        </property>
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>'''
-
-#~ Orden de los valores que se reemplazaran en la plantilla: <checkbox>
-#~ 1- Nombre del checkbox
-#~ 2- Etiqueta del checkbox
-
-checkbox = '''<item>
-       <widget class="QCheckBox" name="chk###">
-        <property name="text">
-         <string>###</string>
-        </property>
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>'''
-
-# diccionario que contiene el fuente de los distintos widgets
-source_widgets = {
-'QCheckBox':checkbox,
-'QRadioButton':radiobutton,
-'QComboBox':combobox,
-'QLineEdit':lineedit,
-}
+import prefijos_widgets
+import xml_widgets
 
 ############################## Funciones ###############################
 
 def __palabra_mas_larga(palabra1, palabra2):
+    """
+    Obtiene la longitud del nombre mas largo de los widgets que se estan usando.
+    A partir de esta longitud se setean despues la lingitud de los labels en la GUI.
+    """
     return palabra1 if len(palabra1) > len(palabra2) else palabra2
 
 def __guardarUI(ruta, contenido):
@@ -226,12 +40,13 @@ def __guardarUI(ruta, contenido):
     return True
 
 def __obtenerEventoCerrarVentana(nombre_boton, tipo_ventana) :
-    resultado = evento_cerrar_ventana.replace('###',nombre_boton)
-    resultado = resultado.replace('%%%',tipo_ventana)
+    resultado = xml_widgets.evento_cerrar_ventana.replace('#nombre_boton#',nombre_boton)
+    resultado = resultado.replace('#tipo_ventana#',tipo_ventana)
     return resultado
     
 def __obtenerTextoBotones(botones) : 
     """
+    Segun el tipo indicado, devuleve el texto que se seteara en los botones.
     """
     boton1, boton2 = '',''
     if 'bt_salir_aceptar' in botones.keys():
@@ -253,32 +68,56 @@ def __generarBotones(codigo, botones = {}):
     if botones : 
         # obtiene que botones se van a generar
         botonA, botonB = __obtenerTextoBotones(botones)
-        resultado = par_botones.replace('###',botonA)
+        resultado = xml_widgets.par_botones.replace('###',botonA)
         resultado = resultado.replace('%%%',botonB)
         return resultado
     else:
         return codigo
     pass
     
+
+def __getXmlWidget(campos):
+    """
+    Segun el tipo de widget, devuelve el XML correspondiente que representa
+    ese widget en una gui Qt. 
+    """
+    # si este campo posee referencia, agrega el boton junto al lineedit
+    xml_widget = ''
+    if (campos['widget_type'] == 'QLineEdit') and (campos['reference'] == True):
+        xml_widget = xml_widgets.source_widgets['QLineEditWithReference']
+    else:
+        xml_widget = xml_widgets.source_widgets[campos['widget_type']]
+    return xml_widget
+
 def __generarWidgets(dic_campos, botones = {}):
     """
     Genera el codigo xml correspondiente a los campos indicados.
     """
     
+    # el diccionario que se recibe, cada elemento tiene este formato
+    # {'atribute':'apellido','widget_type':'QLineEdit','reference':False},
+    
     codigo_widgets = ''
     tamano_layout = 20 # se incrementa de a 40 pixeles
         
-    # con esto se obtiene la longitud del label para que quede todo ordenadito
+    # con esto se obtiene la longitud del label para que quede todo ordenadito :)
     longitud_labels = __obtenerLongitudLabels(dic_campos)    
     cant_campos = len(dic_campos)
     for nro_campo in range(cant_campos) :
         campos = dic_campos[nro_campo] 
-        campo = self.__normalizarNombreCampo( campos.keys()[0] )
+        print campos
+        nombre_campo = __normalizarNombreCampo( campos['atribute'] )
+        nombre_widget = __normalizarNombreWidget( campos['atribute'] )
         # primero, obtiene el xml correspondiente al tipo de widget actual, 
         # y lo mezcla con el del label
-        # luego, reemplaza los ### por el nombre actual del campo
-        xml_widget = source_widgets[campos[campo]]
-        widget_actual = (par_label_layout % (longitud_labels, xml_widget)).replace('###',campo.capitalize())
+        # luego, reemplaza los %--% por el nombre actual del campo
+        
+        xml_widget = __getXmlWidget(campos)
+        widget_actual = (xml_widgets.par_label_layout % (longitud_labels, xml_widget))            
+        # si este widget, posee nombre de campo, lo reemplaza   
+        if widget_actual.find(u'#nombre_campo#') != -1 :
+            widget_actual = widget_actual.replace(u'#nombre_campo#',nombre_campo)
+        widget_actual = widget_actual.replace(u'#nombre_widget#',nombre_widget)
         tamano_layout += 40 # incrementa para  ubicar el proximo widget
         codigo_widgets +=  widget_actual + '\n'
 
@@ -295,7 +134,7 @@ def __generarPlantilla(destino, tipo, metodos = None):
     nombre_archivo = os.path.basename(destino).split('.')[0] + '.py'
     ruta_destino = pathtools.convertPath(os.path.dirname(destino)+'/'+nombre_archivo)
     shutil.copyfile(
-            pathtools.convertPath( pathtools.getPathProgramFolder()+'api/plantilla.py' ),
+            pathtools.convertPath( pathtools.getPathProgramFolder()+'/plantillas/plantilla.py' ),
             ruta_destino)
     
     archivo = open(ruta_destino,'r')
@@ -321,15 +160,25 @@ def __obtenerLongitudLabels(campos) :
 def __obtenerAltoVentana(campos):
     return int((len(campos.keys()) * 40) * 1.5)
     
-def __normalizarNombreCampo(self, nombre):
+def __normalizarNombreCampo(nombre):
+    """
+    Verifica que el nombre no contenga espacios.
+    """
+    while nombre.find('  ') != -1 :
+        nombre.replace('  ',' ')
+    nombre = nombre.capitalize()
+    return nombre
+    
+def __normalizarNombreWidget(nombre):
     """
     Verifica que el nombre no contenga espacios.
     """
     while nombre.find('  ') != -1 :
         nombre.replace('  ',' ')
     nombre = nombre.replace(' ','_')
+    nombre = nombre.capitalize()    
     return nombre
-    
+
 def generarUI(  destino, 
                 campos,                  
                 botones = {}, 
@@ -338,6 +187,11 @@ def generarUI(  destino,
     """ 
     Genera un archivo .ui con los datos que recibe del diccionario campos.     
     campos = {'nombre_campo':tipo_widget}
+    
+    destino = ruta donde se genera el ui
+    campos = diccionario que contiene la informacion de los campos para generar la gui
+    botones = tipo de boton que se agregara a la ventana
+    opciones = diccionario con opciones extra que se involucren en la generacion del ui
     """
     ### Atributos    
     ancho_ventana = 400 
@@ -348,7 +202,7 @@ def generarUI(  destino,
     opciones_generacion = opciones.keys()
    
     # reemplaza el ancho
-    resultado = todo.replace('%ancho%',str(ancho_ventana))
+    resultado = xml_widgets.todo.replace('%ancho%',str(ancho_ventana))
     # reemplaza el alto
     resultado = resultado.replace('%alto%',str(alto_ventana))
     # reemplaza los widgets
@@ -367,7 +221,7 @@ def generarUI(  destino,
     __guardarUI(destino,resultado)
     
     # genera la plantilla para levantar el ui
-    if opciones['generar_plantilla'] == True :
+    if ('generar_plantilla' in opciones) and (opciones['generar_plantilla'] == True) :
         __generarPlantilla(destino,opciones['tipo'])
 
     print '>>> Archivo .ui generado.'
@@ -375,14 +229,21 @@ def generarUI(  destino,
 
 ################################ Extras ################################
 
-def prueba(campos):
-    return __generarPlantilla('/media/Data/ProyectosOn/GUIMaker/src/api/mike.ui','MainWindow')
+#def prueba(campos):
+#    return __generarPlantilla('/media/Data/ProyectosOn/GUIMaker/src/api/mike.ui','MainWindow')
     
+#campos_ejemplo = {
+#'apellido':'QLineEdit',
+#'nombre':'QLineEdit',
+#'dni':'QLineEdit',
+#'direccion':'QLineEdit'
+#}
+
 campos_ejemplo = {
-'apellido':'QLineEdit',
-'nombre':'QLineEdit',
-'dni':'QLineEdit',
-'direccion':'QLineEdit'
+0:{'atribute':'apellido','widget_type':'QLineEdit','reference':True},
+1:{'atribute':'nombre','widget_type':'QLineEdit','reference':False},
+2:{'atribute':'dni','widget_type':'QLineEdit','reference':False},
+3:{'atribute':'direccion','widget_type':'QLineEdit','reference':False}
 }
 
 opciones_ejemplo = {
@@ -393,10 +254,9 @@ botones_ejemplo = {
 'bt_salir_guardar':True
 }
 
-#~ generarUI('/home/emiliano/git/guimaker/src/prueba001.ui',
-    #~ campos_ejemplo,
-    #~ plantilla = True,
-    #~ opciones = opciones_ejemplo,
-    #~ botones = botones_ejemplo)
+#generarUI('/home/mike/prueba001.ui',
+#campos_ejemplo,
+#opciones = opciones_ejemplo,
+#botones = botones_ejemplo)
 
 #~ print prueba(pru)
