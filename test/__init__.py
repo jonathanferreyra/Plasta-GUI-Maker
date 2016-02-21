640edf13db40d7878584bf20a5cd80dc699a03bf
Jonathan Ferreyra <jalejandroferreyra@gmail.com>
2016-02-21 17:32:09 -0300

    added file run_test.py

--- test/__init__.py
+++ test/__init__.py
@@ -2,24 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from test.testapi import TestAPIMaker
-import test.gui 
- 
+from test.test_add_maker import TestAddMaker
+
 def suite():
 
     suite = unittest.TestSuite()
-    
+
     all_test = [
-    TestAPIMaker        
+        TestAddMaker
     ]
     for test in all_test :
-        suite.addTest(unittest.makeSuite(test))   
-    
+        suite.addTest(unittest.makeSuite(test))
+
     return suite
 
 def main():
-    test.gui.main()
     unittest.TextTestRunner(verbosity=2).run(suite())
-    
+
 if __name__ == "__main__":
     main()
